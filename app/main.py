f6ce9c00dcc661166c72d6859089639231075d3a
guog <guog@live.cn>
2025-05-19 17:29:20 +0800

    feat: add healthcheck

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import image2hmi, text2hmi
+from .routers import image2hmi, text2hmi, utils
 
 # Load environment variables
 load_dotenv()
@@ -23,3 +23,4 @@ app.add_middleware(
 # Include API router
 app.include_router(text2hmi.router)
 app.include_router(image2hmi.router)
+app.include_router(utils.router)
